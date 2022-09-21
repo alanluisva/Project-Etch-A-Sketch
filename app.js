d39a90da636aeedb5bca9d23693e2a0601162428
alanluis <alan.luisva@gmail.com>
2022-09-21 17:38:55 -0500

    Set up a hover effect so the divs change color when your mouse passes

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const container = document.querySelector("#container")
+const container = document.querySelector("#container");
 
 for (let i = 0; i < 256; i++) {
-    let div = document.createElement('div');
-    div.classList.add('square');
-    container.appendChild(div)
+    let div = document.createElement("div");
+    div.classList.add("square");
+    container.appendChild(div);
 }
+
+const divs = document.querySelectorAll(".square");
+
+divs.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "black";
+    });
+});
