7d37e32ef48d96db7eb6caea16e8b56a83b637e6
alanluis <alan.luisva@gmail.com>
2022-09-21 21:19:28 -0500

    Create a button that generated a new grid

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const container = document.querySelector("#container");
+const button = document.querySelector("button");
 
 for (let i = 0; i < 256; i++) {
     let div = document.createElement("div");
@@ -6,10 +7,39 @@ for (let i = 0; i < 256; i++) {
     container.appendChild(div);
 }
 
-const divs = document.querySelectorAll(".square");
+let divs = document.querySelectorAll(".square");
 
 divs.forEach((div) => {
     div.addEventListener("mouseover", () => {
         div.style.backgroundColor = "black";
     });
 });
+
+function createGrid() {
+    let squares = Number(prompt("How many squares per side? (Maximum: 100)"));
+    container.textContent = "";
+
+    while (squares > 100 || squares < 0 || !Number.isInteger(squares)) {
+        squares = Number(
+            prompt("You must enter a positive integer. (Maximum: 100) ")
+        );
+    }
+
+    for (let i = 0; i < squares * squares; i++) {
+        let div = document.createElement("div");
+        div.classList.add("square");
+        let side = `${608 / squares}`;
+        div.style.height = `${side}px`;
+        div.style.width = `${side}px`;
+        container.appendChild(div);
+    }
+
+    let divs = document.querySelectorAll(".square");
+    divs.forEach((div) => {
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "black";
+        });
+    });
+}
+
+button.addEventListener("click", createGrid);
